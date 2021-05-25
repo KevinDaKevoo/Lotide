2a9a3b1c0b88cf8cb09b0505e7373bc362fa2270
Kevin Yu <Kevin.yu.kfy@gmail.com>
2021-05-25 21:07:30 +0000

    Added assertArraysEqual.js

---
+++ assertArraysEqual.js
@@ -0,0 +1,42 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function (a, b) {
+  for (let i = 0; i < a.length; i++) {
+    if(a[i] !== b[i]) {
+      return false; 
+    }
+  }
+  return true;
+}
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual([1,2,3], [1,2,3]);
+
+let answer1 = eqArrays([1, 2, 3], [1, 2, 3]); // => true
+let answer2 = eqArrays([1, 2, 3], [3, 2, 1]); // => false
+// you can do this too
+console.log('answer1', answer1);
+console.log('answer2', answer2);
+
+let answer3 = eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
+let answer4 = eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
+console.log('answer3', answer3);
+console.log('answer4', answer4);
+
+
+// ya 
+// rather than manually console logging it out
+const assertArraysEqual = function (x, y) {
+  if (eqArrays(x, y) === true) { 
+    console.log(`✅✅✅ Assertion passed! ${x} === ${y}`);
+  } else {}
+  console.log(`🛑🛑🛑 Assertion Failed! ${x} !== ${y}`);
+}
+assertArraysEqual([1,2,3],[1,2,3])
