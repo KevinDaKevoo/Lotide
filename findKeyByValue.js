983b4e6f7fb93f3cc3bf07ea683e481a58d2b6b5
Kevin Yu <Kevin.yu.kfy@gmail.com>
2021-05-26 16:43:51 +0000

    Added findKeyByValue.js

---
+++ findKeyByValue.js
@@ -0,0 +1,29 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+
+const findKeyByValue = function(show, match) {
+  const arrayOfGenre = Object.keys(show);
+  for (let genre of arrayOfGenre) {
+    if (show[genre] === match) {
+      return genre;
+    }
+  }
+};
+
+
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "comedy");
