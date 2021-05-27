831deb41c4c320afe2dffc2719d25ec909075f79
Kevin Yu <Kevin.yu.kfy@gmail.com>
2021-05-27 20:38:11 +0000

    Added findKey.js

---
+++ findKey.js
@@ -0,0 +1,18 @@
+const findKey = (keys, action) => {
+  const arrayKeys = Object.keys(keys);
+  for (let key of arrayKeys) {
+    if (action(keys[key])) {
+      return key;
+    }
+  }
+};
+
+console.log(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2) // => "noma"
+);
