37c688774c73c56e0284350dc1363803c4585459
Kevin Yu <Kevin.yu.kfy@gmail.com>
2021-05-31 13:21:56 +0000

    Added headTest.js and assertEqualTest.js, also updated assertEqual.js and head.js to export and require

--- head.js
+++ head.js
@@ -1,19 +1,9 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+const assertEqual = require('./assertEqual');
+const head = function(x) {
+  if (x.length > 0) {
+    return x[0];
   } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
-const head = function(x) {
-  if (x.length > 0) {
-  return x[0]
-  } else {
-    
-  };
-};
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+module.exports = head;
---
+++ test/headTest.js
@@ -0,0 +1,6 @@
+const head = require("../head");
+
+console.log(head([5,6,7]));
+console.log(head(["Hello", "Lighthouse", "Labs"]));
+console.log(head("Lighthouse Labs", "Bootcamp"));
+console.log(head(1));
