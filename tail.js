e0dba7fa2d05909a0f0133e2c91f0f7451b7a5cb
Kevin Yu <Kevin.yu.kfy@gmail.com>
2021-05-31 13:36:51 +0000

    removed extra code from tail.js

--- tail.js
+++ tail.js
@@ -1,16 +1,5 @@
-const assertEqual = require('./assertEqual');
-
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-
-
 const tail = function(y) {
   return y.slice(1);
 };
 
-
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
 module.exports = tail;
